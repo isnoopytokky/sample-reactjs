406bc8fb64ddc5fad0a75600f4d24dc11370509e
Pariwat Ramphuengnit <tokpower2@gmail.com>
2019-05-20 16:36:29 +0700

    Update cfg.sh

--- lib/cfg.sh
+++ lib/cfg.sh
@@ -1,8 +1,8 @@
 echo 'start load local libs'
 pwd
-export SASS_BINARY_PATH=./lib/linux-x64-57_binding.node
+export SASS_BINARY_PATH= /opt/app-root/src/lib/linux-x64-57_binding.node
 ls ./lib/
-#npm install -g node-sass
+npm install -g node-sass
 #--sass-binary-path="C:\\Users\\myuser\\Downloads\\win32-x64-48_binding.node"
-npm install -g node-sass --sass-binary-path="./lib/linux-x64-57_binding.node"
+#npm install -g node-sass --sass-binary-path="./lib/linux-x64-57_binding.node"
 echo 'finished load local libs'
